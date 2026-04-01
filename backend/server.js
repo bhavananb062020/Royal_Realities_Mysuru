149a87c4474b066545db78bbc5a6ebfcbae9bd38
bhavananb062020 <bhavananb2003@gmail.com>
2026-04-01 19:07:04 +0530

    changed in server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(cors({
     "https://royalrealitiesmysuru.pages.dev"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
