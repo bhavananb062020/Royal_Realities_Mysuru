f9d56776396a3615d61138447ac032622c70962f
bhavananb062020 <bhavananb2003@gmail.com>
2026-04-02 17:34:30 +0530

    Changed Tagline alignment

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -163,8 +163,9 @@ const HeroSection = () => {
 
       {/* HERO TEXT */}
       <div className="relative md:absolute md:top-[35%] left-6 md:left-16 z-20 text-white mt-32 md:mt-0 max-w-xl">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-wide">
-          INVEST IN YOUR <span className="font-light">FUTURE HOME</span>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-wide leading-tight">
+          INVEST IN YOUR
+          <span className="block font-light mt-2">FUTURE HOME</span>
         </h1>
       </div>
 
