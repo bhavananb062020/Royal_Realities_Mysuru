789a2b08955b63e1eada13c6357df917f8c23dfc
bhavananb062020 <bhavananb2003@gmail.com>
2026-04-02 12:21:21 +0530

    changed checkbox border color

--- frontend/src/components/ui/checkbox.jsx
+++ frontend/src/components/ui/checkbox.jsx
@@ -8,7 +8,7 @@ const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-white shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-white data-[state=checked]:text-black",
+      "peer h-4 w-4 shrink-0 rounded-sm border border-black shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-white data-[state=checked]:text-black",
       className
     )}
     {...props}
